d4c45f24dbcebae279213dde3037b8011c45e793
samit-maersk <SamitKumar.Patel@maersk.com>
2023-07-02 22:43:20 +0200

    reactor-kafka example with avro schema

--- README.md
+++ README.md
@@ -19,7 +19,9 @@ An example of using Spring WebFlux and reactor Kafka:
 
 - [producer](https://github.com/reactor/reactor-kafka/blob/main/reactor-kafka-samples/src/main/java/reactor/kafka/samples/SampleProducer.java)
 - [consumer](https://github.com/reactor/reactor-kafka/blob/main/reactor-kafka-samples/src/main/java/reactor/kafka/samples/SampleConsumer.java)
-
+- [avro example](https://www.confluent.io/blog/schema-registry-avro-in-spring-boot-application-tutorial/)
+- [avro example gitgub](https://github.com/confluentinc/springboot-kafka-avro/tree/master/src)
+- [avro example](https://github.com/lydtechconsulting/kafka-schema-registry-avro/blob/v1.0.0/schema-registry-demo-service/src/main/java/demo/kafka/KafkaDemoConfiguration.java)
 - To see the messages in Kafka topic:
 
 ```shell
--- src/main/java/com/example/webfluxkafka/WebfluxKafkaApplication.java
+++ src/main/java/com/example/webfluxkafka/WebfluxKafkaApplication.java
@@ -1,32 +1,42 @@
 package com.example.webfluxkafka;
 
+import io.confluent.developer.User;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
-import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.receiver.ReceiverRecord;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 import reactor.kafka.sender.SenderRecord;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Collections;
@@ -40,7 +50,10 @@ public class WebfluxKafkaApplication {
 	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServers;
 
-	@Value("${spring.kafka.topic}")
+	@Value("${spring.kafka.schema.registry.url}")
+	private String schemaRegistry;
+
+	@Value("${spring.kafka.topic.topic1}")
 	private String topicName;
 
 	public static void main(String[] args) {
@@ -59,70 +72,94 @@ public class WebfluxKafkaApplication {
 	public NewTopic topic1() {
 		return new NewTopic(topicName, 1, (short) 1);
 	}
-
-
 	@Bean
-	KafkaSender<Integer, String> kafkaSender() {
-		Map<String, Object> producerProps = new HashMap<>();
-		producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-		producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
-		producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-		SenderOptions<Integer, String> senderOptions =
-				SenderOptions.<Integer, String>create(producerProps)
-						.maxInFlight(1024);
-
-		return KafkaSender.create(senderOptions);
+	public NewTopic bank(@Value("${spring.kafka.topic.bank}") String topicName) {
+		return new NewTopic(topicName, 1, (short) 1);
+	}
+	@Bean
+	public NewTopic user(@Value("${spring.kafka.topic.user}") String topicName) {
+		return new NewTopic(topicName, 1, (short) 1);
 	}
 
-
 	@Bean
-	ApplicationListener<ApplicationReadyEvent> ready() {
+	ApplicationListener<ApplicationReadyEvent> ready(UsersRepository usersRepository) {
 		return event -> {
+			//Consumer 1
 			Map<String, Object> consumerProps = new HashMap<>();
 			consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 			consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "sample-group");
-			consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
-			consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+			consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+			consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+			consumerProps.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistry);
 
-			ReceiverOptions<Integer, String> receiverOptions =
-					ReceiverOptions.<Integer, String>create(consumerProps)
+//			consumerProps.put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
+//			consumerProps.put(AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG, schemaRegistryUsername + ":" + schemaRegistryPassword);
+
+			ReceiverOptions<String, User> receiverOptions =
+					ReceiverOptions.<String, User>create(consumerProps)
+							.pollTimeout(Duration.ofMillis(5000))
 							.subscription(Collections.singleton(topicName));
 
-			Flux<ReceiverRecord<Integer, String>> inboundFlux =
+			Flux<ReceiverRecord<String, User>> inboundFlux =
 					KafkaReceiver.create(receiverOptions)
 							.receive();
 
-			inboundFlux.subscribe(r -> {
-				log.info("Received message: {}\n", r);
-				r.receiverOffset().acknowledge();
-			});
+			inboundFlux
+					// store this in the db. If success r.receiverOffset().acknowledge() or else don't clear the offset..rather retry
+					.subscribe(r -> {
+						log.info("Received message: {}\n", r);
+						r.receiverOffset().acknowledge();
+					});
+			//Consumer 1
 		};
 	}
 
 	//route
 	@Bean
-	RouterFunction routes(KafkaSender sender) {
+	RouterFunction routes() {
 		return RouterFunctions.route()
 				.POST("/send-message", request -> {
 					return request.bodyToMono(String.class)
-							.doOnNext(msg -> sendMessage(sender, topicName))
+							.doOnNext(msg -> sendMessage(topicName))
 							.then(ServerResponse.ok().build());
 				})
 				.build();
 	}
 
-	private void sendMessage(KafkaSender sender, String topicName) {
+	private void sendMessage(String topicName) {
+
+		// This can be a generic bean if you have one producer type
+		Map<String, Object> producerProps = new HashMap<>();
+		producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+		producerProps.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistry);
+
+//		producerProps.put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
+//		producerProps.put(AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG, schemaRegistryUsername + ":" + schemaRegistryPassword);
+//		producerProps.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
+
+		SenderOptions<String, User> senderOptions =
+				SenderOptions.<String, User>create(producerProps)
+						.maxInFlight(1024);
+
+		KafkaSender<String, User> kafkaSender = KafkaSender.create(senderOptions);
+		// This can be a generic bean if you have one producer type
+
 		var partition = 0;
 		var currentTimeInLong = LocalDateTime.now().atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
+		Flux<SenderRecord<String, User, Integer>> outboundFlux = Flux.range(1, 5)
+				.map(i -> SenderRecord.create(topicName, partition, currentTimeInLong , "Message_" + i, User.newBuilder().setName("Name "+i).setAge(20 + i).build(), i));
 
-		Flux<SenderRecord<Integer, String, Integer>> outboundFlux = Flux.range(1, 10)
-				.map(i -> SenderRecord.create(topicName, partition, currentTimeInLong , i, "Message_" + i, i));
-
-		sender.send(outboundFlux)
+		kafkaSender.send(outboundFlux)
 				.doOnError(e-> log.error("Send failed", e))
 				.doOnNext(r -> log.info("Message Sent" ))
 				.subscribe();
 	}
 
 }
+
+@Document
+record Users(@Id String id, String name, int age){}
+
+interface UsersRepository extends ReactiveMongoRepository<Users,String>{}
