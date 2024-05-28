c129932035fcb45e9c7fff49f9f7a45dbb412555
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2024-05-28 16:51:01 +0200

    Test added

--- src/test/java/com/example/webfluxkafka/WebfluxKafkaApplicationTests.java
+++ src/test/java/com/example/webfluxkafka/WebfluxKafkaApplicationTests.java
@@ -1,40 +1,35 @@
 package com.example.webfluxkafka;
 
-import org.junit.Ignore;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
 @Testcontainers
-@Ignore
 class WebfluxKafkaApplicationTests {
 
 	static Network network = Network.newNetwork();
+
 	@Container
 	@ServiceConnection
-	static MongoDBContainer MONGODB = new MongoDBContainer(DockerImageName.parse("mongo"))
+	static MongoDBContainer MONGODB = new MongoDBContainer(DockerImageName.parse("mongo:4.4.2"))
 			.withNetwork(network)
 			.waitingFor(Wait.forListeningPort());
 
 	@Container
-	static KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka").asCompatibleSubstituteFor("apache/kafka"))
+	static org.testcontainers.containers.KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))
 			.withNetwork(network)
 			.withExposedPorts(9093)
 			.withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9093 ,BROKER://0.0.0.0:9092")
@@ -47,7 +42,7 @@ class WebfluxKafkaApplicationTests {
 			.withExposedPorts(8081)
 			.withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema-registry")
 			.withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:8081")
-			.withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", KAFKA.getNetworkAliases().get(0)+":9092")
+			.withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS", KAFKA.getNetworkAliases().getFirst()+":9092")
 			.waitingFor(Wait.forListeningPort());
 
 	@Autowired
@@ -56,31 +51,4 @@ class WebfluxKafkaApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-	@DisplayName("Test /send-message")
-	void e2eTest() {
-		assertAll("e2e test",
-				() -> webTestClient
-						.post()
-						.uri("/send-message")
-						.contentType(MediaType.APPLICATION_JSON)
-						.bodyValue("""
-								{
-									"content": "Hello, Kafka!"
-								}
-								""")
-						.exchange()
-						.expectStatus().isOk(),
-				() -> webTestClient
-						.get()
-						.uri("/db")
-						.exchange()
-						.expectStatus().isOk()
-						.expectBodyList(DbMessage.class)
-						.hasSize(1)
-		);
-	}
-
-
 }
